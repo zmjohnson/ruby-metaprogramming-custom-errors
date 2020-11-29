83aa5244839a7d4dcedfa14292efe5400d6523fa
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-11-28 19:12:01 -0500

    commit tree baedf11c86855a8544fa31e3a9fb8e22c669e1ab

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,24 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+
+  class PartnerError < StandardError
+    #Raise the PartnerError when the argument passed into get_married
+    #is not an instance of the Person class
+    def message
+      "you must give the get_married method an argument of
+      an instance of the person class!"
+    end
   end
 
 end
@@ -15,7 +32,3 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
